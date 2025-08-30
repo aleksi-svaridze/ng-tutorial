7dd132aa2a375b83a30bff6c0452f7717ce46a5a
aleksi-svaridze <aleksi.svaridze82@gmail.com>
2025-08-30 19:00:04 +0400

    Add new branch - typescript

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('ng-tutorial');
+  protected readonly title = signal('ng-tutorial: week 1: Typescript');
 }
